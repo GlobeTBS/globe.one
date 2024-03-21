89490eb0138859851e252be0f34214234513583f
Globe <162892913+GlobeTBS@users.noreply.github.com>
2024-03-21 16:29:13 +1100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const overlay = document.getElementById("overlay")
     const enterButton = document.getElementById("enterButton")
     const backgroundVideo = document.getElementById("backgroundVideo")
-    const theme = "central"
+    const theme = "chill"
 
     backgroundVideo.controls = false
 
     if (theme == "chill") {
-        backgroundVideo.src = "https://cdn.discordapp.com/attachments/1211139202104696875/1214921211344195614/song_1.mp4?ex=65fade8c&is=65e8698c&hm=975a1531e82a2748c1ad4e9a467af18aff221b6c47ea275b262dfec14fa238dc&"
+        backgroundVideo.src = "https://cdn.discordapp.com/attachments/1211139202104696875/1214921211344195614/song_1.mp4?ex=660d538c&is=65fade8c&hm=1d03fd7c2e81c393fb170b239ddc349dbb55ae783694906b3fb8c39f8a121a9b&"
     } 
 
     if (theme == "central") {
